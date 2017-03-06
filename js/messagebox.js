11a046b36297a661930efbfa1783c855658d8f3f
余宝彬 <yohouakira@yubaobindeMacBook-Pro.local>
2017-03-06 17:47:49 +0800

    添加回调函数

--- js/messagebox.js
+++ js/messagebox.js
@@ -6,7 +6,6 @@
  */
 ;(function(global) {
     "use strict";
-
     global = global ? global : window;
 
     let MsgBox = function(el,options) {
@@ -14,35 +13,35 @@
     };
 
     MsgBox.prototype = {
-        _init: function(el,options) {
+        _init: function(el,options,callback_show,callback_hide) {
             let me = this;
-            let $el = el;
+            me.$el = el;
+            me.callback_show = callback_show;
+            me.callback_hide = callback_hide;
             if(el){
-                me.msgbox = _createOneEl('div',$el); 
+                me.msgbox = _createOneEl('div',me.$el); 
             }else {
                 me.msgbox = _createEl('div'); 
             }
             //默认选项
             me.options = {
-                width:'150px',
                 height:'30px',
                 message: "",
-                bg:"rgba(39,174,96, 0.6)",
-                color:"#FFF",
-                left:"36%",
+                bgColor:"rgba(39,174,96, 0.6)",
+                color:"#FFF",                
                 top:"40%",
                 fontSize:'16px',
                 lineHeight:'30px',
-                autoHide:2000,
-                remove:false,
+                autoHide:false,
                 border:""
             }
             for (let i in options) {
                 me.options[i] = options[i];
             }
-            if(!$el) {
-                me.options['remove'] = true;
+            if(!me.$el) {
+                throw new Error("Can not find the element!");
             }
+            me.msgbox.style.width = '100%';
             me.msgbox.style.position = 'fixed';
             me.msgbox.style.textAlign = 'center';
             me.msgbox.style.display = 'none';
@@ -53,15 +52,11 @@
 
         _refresh: function(){
             let me = this;
-            me.msgbox.style.width = me.options.width;
-            me.msgbox.style.left = me.options.left;
             me.msgbox.style.top = me.options.top;
-            me.msgbox.style.backgroundColor = me.options.bg;
             me.msgbox.style.color = me.options.color;
             me.msgbox.style.fontSize = me.options.fontSize;
             me.msgbox.style.lineHeight = me.options.lineHeight;
-            me.msgbox.style.border = me.options.border;
-            me.msgbox.innerHTML = me.options.message;
+            me.msgbox.innerHTML = `<span style="padding:5px 10px;background-color:${me.options.bgColor};">${me.options.message}</span>`;
         },
 
         show: function(){
@@ -69,14 +64,15 @@
             me.msgbox.style.display = '';
             setTimeout(function(){
                 me.msgbox.style.opacity = 1;
-            },20)
-            setTimeout(function(){
-                if(me.options.remove){//移除元素
-                    me.remove(me.msgbox);
-                }else if(me.options.autoHide){//自动隐藏
-                    me.hide();
+                if(me.options['shown'] && typeof me.options['shown'] == "function"){
+                    me.options['shown'].call(null);
                 }
-            },me.options.autoHide);
+            },20)
+            if(me.options.autoHide){//自动隐藏
+                setTimeout(function(){
+                    me.hide();
+                },2000);
+            }
         },
         //隐藏元素
         hide: function(){
@@ -84,6 +80,9 @@
             me.msgbox.style.opacity = 0;
             setTimeout(function(){
                 me.msgbox.style.display = 'none';
+                if(me.options['hidden'] && typeof me.options['hidden'] == "function"){
+                    me.options['hidden'].call(null);
+                }
             },600)
         },
         //移除元素
@@ -92,6 +91,27 @@
             setTimeout(function(){
                 document.querySelector('body').removeChild(el);
             },600);
+        },
+        setOption: function(options){
+            let me = this;
+            for (let i in options) {
+                me.options[i] = options[i];
+            }
+            me._refresh();
+        },
+        
+        //绑定事件
+        on: function(name,callback){
+            let me = this
+            if(callback && typeof callback == "function"){
+                me.options[name] = callback;
+            }
+        },
+
+        //解除绑定
+        off: function(name){
+            let me = this;
+            me.options[name] = "";
         }
     };
 
@@ -106,13 +126,13 @@
         }
         return tag;
     }
-    //创建元素
-    function _createEl(tagName){
-        let tag = document.createElement(tagName);
-        tag.id = 'msg' + Math.random().toString().substr(2, 8);
-        document.querySelector('body').appendChild(tag);
-        return tag;
-    }
+    //创建随机id元素
+    // function _createEl(tagName){
+    //     let tag = document.createElement(tagName);
+    //     tag.id = 'msg' + Math.random().toString().substr(2, 8);
+    //     document.querySelector('body').appendChild(tag);
+    //     return tag;
+    // }
 
     if (typeof module !== 'undefined' && module.exports) module.exports = MsgBox;
     if (typeof define === 'function') define(function() { return MsgBox; });
